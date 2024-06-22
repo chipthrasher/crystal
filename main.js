8d7a43a144bf49d74b287f83bc69c31db60cf268
Chip Thrasher <jjthrash@ncsu.edu>
2024-06-22 10:15:07 -0400

    Game state, images, difficulty

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('header').classList.add('header-ingame');
 });
 
+// GAME STATE consists of an INDEX of the progression array, an INVENTORY of items, and an amount of MONEY.
+
 // Items that can be sold in game:
 const items = {
   "flashlight": {
@@ -16,22 +18,27 @@ const progression = [
   {
     "type": "gameplay",
     "name": "Green Grotto",
-    "description": "You find yourself in the Green Grotto, a lush and verdant cavern. The air is thick with the scent of moss and damp earth. The sound of water dripping echoes through the chamber."
+    "description": "You find yourself in the Green Grotto, a lush and verdant cavern. The air is thick with the scent of moss and damp earth. The sound of water dripping echoes through the chamber.",
+    "image": "assets/green-grotto.jpg",
+    "difficulty": 0
   },
   {
     "type": "gameplay",
     "name": "Down the Hole",
-    "description": "It is darker in here. It is hard to see. You wish you had a torch or some night vision goggles."
+    "description": "It is darker in here. It is hard to see. You wish you had a torch or some night vision goggles.",
+    "difficulty": 0.1
   },
   {
     "type": "gameplay",
     "name": "Twinkling Calcite",
-    "description": "The walls are lined with calcite crystals that twinkle in the dim light. You feel a sense of wonder and awe."
+    "description": "The walls are lined with calcite crystals that twinkle in the dim light. You feel a sense of wonder and awe.",
+    "difficulty": 0.1
   },
   {
     "type": "gameplay",
     "name": "Crystal Caverns",
-    "description": "The cavern opens up into a vast chamber. The walls are lined with crystals that sparkle like diamonds."
+    "description": "The cavern opens up into a vast chamber. The walls are lined with crystals that sparkle like diamonds.",
+    "difficulty": 0.2
   },
   {
     "type": "merchant",
@@ -42,6 +49,7 @@ const progression = [
   {
     "type": "gameplay",
     "name": "Catacombs",
-    "description": "It is dark in here. It is hard to see. You wish you had a torch or some night vision goggles."
+    "description": "It is dark in here. It is hard to see. You wish you had a torch or some night vision goggles.",
+    "difficulty": 0.4
   }
 ];
