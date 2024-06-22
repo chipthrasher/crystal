25cd0db7207807f24be6352fa4934e0f2397fbcd
Chip Thrasher <jjthrash@ncsu.edu>
2024-06-22 01:00:08 -0400

    Wireframe progress

---
+++ main.js
@@ -0,0 +1,4 @@
+// When document ready using vanilla JS, add class "header-main-menu" to #header
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('header').classList.add('header-main-menu');
+});
