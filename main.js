78db906b8558a60b346d5d756be14b7575097ac9
Chip Thrasher <jjthrash@ncsu.edu>
2024-06-22 09:46:25 -0400

    Ratfolk Merchant prototype

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // When document ready using vanilla JS, add class "header-main-menu" to #header
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('header').classList.add('header-main-menu');
+//   document.getElementById('header').classList.add('header-main-menu');
+  document.getElementById('header').classList.add('header-ingame');
 });
